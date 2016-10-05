9cc7fa75d2f46d435f6fb4a7b398cfad1a903515
dtice <dillon.tice@gmail.com>
2016-10-05 14:48:36 -0600

    Main.java Update

--- Lab-2/src/Main.java
+++ Lab-2/src/Main.java
@@ -1,4 +1,47 @@
+import java.util.Scanner;
 
-public class Main {
-//Lets see if this shit works.
+public class notMain 
+{
+	public static void main(String[] args)
+	{
+		
+	}
 }
+
+class hashTree
+{
+	hashTree()
+	{
+		
+	}
+}
+
+class hashMap
+{
+	int tableSize;
+	int itemNum;
+	hashTree[] table;
+	
+	hashMap(int tableSize)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter size of hash table: ");
+		tableSize = in.nextInt();
+		System.out.println("Enter initial number of items: ");
+		itemNum = in.nextInt();
+		in.close();
+	}
+	
+	void insert(int key)
+	{
+		
+	}
+	
+	void search(int key)
+	{
+		int hash = (key % tableSize);
+	}
+	
+	
+}
+
