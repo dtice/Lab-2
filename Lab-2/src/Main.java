b88465fc8719cd6e56bc56950a395a49acffd05a
DillT <DillT@DESKTOP-862N167>
2016-10-05 15:14:48 -0600

    Layout for Lab 2

--- Lab-2/src/Main.java
+++ Lab-2/src/Main.java
@@ -1,16 +1,54 @@
 import java.util.Scanner;
 
-public class notMain 
+public class Main 
 {
 	public static void main(String[] args)
 	{
-		
+		hashMap hm = new hashMap();
+	}
+}
+
+class Node
+{
+	int key;
+	int value;
+	Node left;
+	Node right;
+	
+	Node()
+	{}
+	
+	Node(int key, int value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+	
+	public int getKey()
+	{
+		return key;
+	}
+	
+	public int getValue()
+	{
+		return value;
 	}
 }
 
 class hashTree
 {
+	Node root;
 	hashTree()
+	{
+		root = new Node();
+	}
+	
+	public void find()
+	{
+		
+	}
+	
+	public void print()
 	{
 		
 	}
@@ -22,7 +60,7 @@ class hashMap
 	int itemNum;
 	hashTree[] table;
 	
-	hashMap(int tableSize)
+	hashMap()
 	{
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter size of hash table: ");
@@ -30,6 +68,11 @@ class hashMap
 		System.out.println("Enter initial number of items: ");
 		itemNum = in.nextInt();
 		in.close();
+		table = new hashTree[tableSize];
+		for(int i = 0; i < tableSize; i++)
+		{
+			table[i] = new hashTree();
+		}
 	}
 	
 	void insert(int key)
@@ -37,9 +80,14 @@ class hashMap
 		
 	}
 	
+	/**
+	 * Search function finds hashTree that contains the key, searches the tree for the key, 
+	 * then returns it.
+	 * @param key : value you are searching for
+	 */
 	void search(int key)
 	{
-		int hash = (key % tableSize);
+		
 	}
 	
 	
