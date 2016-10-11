ed585564249f5ab47db6ca846321656a7ca4cd34
DillT <DillT@DESKTOP-862N167>
2016-10-10 18:56:56 -0600

    Insert method in HashTree class is now working.

--- Lab-2/src/Main.java
+++ Lab-2/src/Main.java
@@ -2,96 +2,119 @@ import java.util.Scanner;
 
 //Sick. Egit is working
 
-public class Main 
-{
-	public static void main(String[] args)
-	{
+public class Main {
+	public static void main(String[] args) {
 		hashMap hm = new hashMap();
 	}
 }
 
-class Node
-{
+class Node {
 	int key;
 	int value;
 	Node left;
 	Node right;
-	
-	Node()
-	{}
-	
-	Node(int key, int value)
-	{
+
+	Node() {
+	}
+
+	Node(int key, int value) {
 		this.key = key;
 		this.value = value;
 	}
-	
-	public int getKey()
-	{
+
+	public int getKey() {
 		return key;
 	}
-	
-	public int getValue()
-	{
+
+	public int getValue() {
 		return value;
 	}
 }
 
-class hashTree
-{
+class hashTree {
 	Node root;
-	hashTree()
-	{
-		root = new Node();
+
+	hashTree() {
+		root = null;
 	}
-	
-	public void search()
-	{
-		
+
+	public void insert(Node n) {
+		if (root == null) {
+			root = n;
+		} else {
+			Node current = root;
+			Node parent = null;
+			while (1 == 1) {
+				parent = current;
+				if (n.key < current.key) {
+					current = current.left;
+
+					if (current == null) {
+						parent.left = n;
+						return;
+					}
+				} else {
+
+					current = current.right;
+					if (current == null) {
+						parent.right = n;
+						return;
+					}
+				}
+			}
+		}
 	}
-	
-	public void print()
-	{
-		
+
+	public void search() {
+
+	}
+
+	public void print(Node root) {
+		if (root != null) {
+			print(root.left);
+			System.out.print(root.value);
+			print(root.right);
+		}
 	}
 }
 
-class hashMap
-{
+class hashMap {
 	int tableSize;
 	int itemNum;
 	hashTree[] table;
-	
-	hashMap()
-	{
+	Node[] nodes = { new Node(1, 1), new Node(2, 2), new Node(3, 3), new Node(4, 4) };
+
+	hashMap() {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter size of hash table: ");
 		tableSize = in.nextInt();
 		System.out.println("Enter initial number of items: ");
 		itemNum = in.nextInt();
 		in.close();
-		table = new hashTree[tableSize];
-		for(int i = 0; i < tableSize; i++)
+		table = new hashTree[tableSize]; // Creates array of trees
+		for (int i = 0; i < tableSize; i++) // Initializes trees in array
 		{
 			table[i] = new hashTree();
 		}
+		for (int i = 0; i < 4; i++) {
+			table[0].insert(nodes[i]);
+		}
+		table[0].print(table[0].root);
 	}
-	
-	void insert(int key)
-	{
-		
-	}
-	
-	/**
-	 * Search function finds hashTree that contains the key, searches the tree for the key, 
-	 * then returns it.
-	 * @param key : value you are searching for
-	 */
-	void search(int key)
-	{
-		
-	}
-	
-	
-}
 
+	void insert(int key) {
+
+	}
+
+	/**
+	 * Search function finds hashTree that contains the key, searches the tree
+	 * for the key, then returns it.
+	 * 
+	 * @param key
+	 *            : value you are searching for
+	 */
+	void search(int key) {
+
+	}
+
+}
